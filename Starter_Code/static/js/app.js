86e56032a25f5b9357e4119f36a524148f4b9924
James Haugen <jaugen@mindspring.com>
2024-05-11 10:06:07 -0700

    Experimenting

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,9 +1,9 @@
-function bigNumbers(number) {
+function otu(number) {
   return otu__ids > 1000;
 }
   
   
-  // Build the metadata panel
+// Build the metadata panel
 // function buildMetadata(sample) {.the
   const dataPromise = d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json");
     console.log(dataPromise);
@@ -18,11 +18,19 @@ function bigNumbers(number) {
     console.log("metadata: ", metadata);    
       
   let names = data.names;
-    console.log("names:", names);        
+    console.log("names: ", names);        
     
   let samples = data.samples;
-    console.log("samples:", samples);   
+    console.log("samples: ", samples);   
 
+  let id = samples.id;
+    console.log("id: ", id);   
+
+  let selDataset = d3.select("#selDataset").text("James");
+    console.log("selDataset: ", selDataset);   
+
+  let sample_metadata = d3.select("#sample-metadata").text("Jim");
+    console.log("sample-metadata says: ", sample_metadata);   
 
   } );      
 
@@ -30,81 +38,15 @@ function bigNumbers(number) {
   console.log("Executed after 5 seconds");
 }, 5000);
  
-//  let mapSimpleArray = data.map(function(item) {
-//   return item;
-// });
-
-// // Make the demographics panel
-// function demo(selectedValue) {
-//   // Fetch the JSON data and console log it
-//   d3.json(url).then((data) => {
-//       console.log(`Data: ${data}`);
-
-//       // An array of metadata objects
-//       let metadata = data.metadata;
-      
-//       // Filter data where id = selected value after converting their types 
-//       // (bc meta.id is in integer format and selectValue from is in string format)
-//       let filteredData = metadata.filter((meta) => meta.id == selectedValue);
-    
-//       // Assign the first object to obj variable
-//       let obj = filteredData[0]
-      
-//       // Clear the child elements in div with id sample-metadata
-//       d3.select("#sample-metadata").html("");
-
-//       // Object.entries() is a built-in method in JavaScript 
-//       // This returns an array of a given object's own enumerable property [key, value]
-//       let entries = Object.entries(obj);
-      
-//       // Iterate through the entries array
-//       // Add a h5 child element for each key-value pair to the div with id sample-metadata
-//       entries.forEach(([key,value]) => {
-//           d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
-//       });
-
-//       // Log the entries Array
-//       console.log(entries);
-//   });
-// }
-
-// /// Create an array of metadata key
-// // let id = Object.values(metadata.id);
-// let ethnicity = Object.values(metadata.ethnicity);
-// let gender = Object.values(metadata.gender);
-// let age = Object.values(metadata.age);
-// let location = Object.values(metadata.location);
-// let wfreq = Object.values(metadata.wfreq);
-// console.log(data);// 
-
-//  get the metadata field
-
-///// create a custom filtering function
-
-// // Display the default plot
-// function init() {
-//   let data = [{
-//     values: australia,
-//     labels: labels,
-//     type: "pie"
-//   }];
-
-//   let layout = {
-//     height: 600,
-//     width: 800
-//   };
-
-//   Plotly.newPlot("pie", data, layout);
-// }})
-
-// function selectTop10;
-
+  // get the metadata field
 
 
     // Filter the metadata for the object with the desired sample number
 
 
     // Use d3 to select the panel with id of `#sample-metadata`
+  let sample_metadata = d3.select("#sample-metadata").text("Jim");
+    console.log("sample-metadata says: ", sample_metadata);   
 
 
     // Use `.html("") to clear any existing metadata
@@ -113,12 +55,12 @@ function bigNumbers(number) {
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
 
-  // });
+//   });
 // }
 
 // function to build both charts
-// function buildCharts(sample) {
-  // d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+function buildCharts(sample) {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
 
@@ -142,40 +84,39 @@ function bigNumbers(number) {
     // Don't forget to slice and reverse the input data appropriately
 
 
-//     // Render the Bar Chart
+    // Render the Bar Chart
 
-//   });
-// }
+  });
+}
 
-// // Function to run on page load
-// function init() {
-//   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+// Function to run on page load
+function init() {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
-//     // Get the names field
-
-
-//     // Use d3 to select the dropdown with id of `#selDataset`
+    // Get the names field
 
 
-//     // Use the list of sample names to populate the select options
-//     // Hint: Inside a loop, you will need to use d3 to append a new
-//     // option for each sample name.
+    // Use d3 to select the dropdown with id of `#selDataset`
 
 
-//     // Get the first sample from the list
+    // Use the list of sample names to populate the select options
+    // Hint: Inside a loop, you will need to use d3 to append a new
+    // option for each sample name.
 
 
-//     // Build charts and metadata panel with the first sample
+    // Get the first sample from the list
 
-//   });
-// }
 
-// // Function for event listener
-// function optionChanged(newSample) {
-// //   // Build charts and metadata panel each time a new sample is selected
+    // Build charts and metadata panel with the first sample
 
-// // }
+  });
+}
 
-// // Initialize the dashboard
-// init()
-//}
+// Function for event listener
+function optionChanged(newSample) {
+  // Build charts and metadata panel each time a new sample is selected
+
+}
+
+// Initialize the dashboard
+init();
